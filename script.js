ce914acb03728502e8a90a8e66995b4659bdf1cf
RobsonLima2 <robson.ramos.lima@hotmail.com>
2022-06-10 17:23:06 -0300

    exercicio estrutura de dados

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Robson"
+let number = 28
+let anoAtual = 2022 - number
+let soma = number + anoAtual
+console.log(soma)
+console.log("Estou mexendo no console.log")
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
